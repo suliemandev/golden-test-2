4ef6ae2c7fada8b38af1d25dbb211c56df64aac7
DESKTOP-225CICE\WIN10 <milad@zvoid.com>
2020-09-20 19:32:35 +0300

    m

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit298f10ad71c10bcbb55a519577b337b5
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
