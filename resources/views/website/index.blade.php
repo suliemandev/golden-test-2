f7199762f83b7fb14c488809b91fd9b2c29b2d45
Sulieman <sulieman@averotech.com>
2020-11-02 21:11:13 +0200

    no message

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -9,18 +9,18 @@
             <h2 class="text-4xl lg:text-6xl text-black text-center">
                 {{ __('Discover Your') }} <span class="font-bold">{{ __('Future') }} </span>
             </h2>
-            <h4 class="text-lg lg:text-2xl text-center mt-4 text-black opacity-75 mb-16">
+            <h4 class="text-lg lg:text-2xl text-center mt-4 text-black opacity-75">
                 {{ __('Intro description') }}
             </h4>
 
-            <div class="flex flex-shrink-0 items-center justify-center">
+            <div class="flex items-center justify-center mb-16 mt-7">
                 
-                <div class="bg-black me-6 flex w-16 h-16 relative border-2 border-black rounded-full z-10 items-center justify-center">
+                <div class="bg-black border-2 border-black flex flex-shrink-0 h-16 items-center justify-center me-4 sm:me-6 relative rounded-full w-16 z-10">
                     <svg class="relative z-10 w-6 h-6 text-yellow-500" fill="currentColor" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><path d="M232.3125,114.34375,88.34375,26.35937A15.99781,15.99781,0,0,0,64,40.00781V215.99219a16.00521,16.00521,0,0,0,24.34375,13.64843L232.3125,141.65625a16.00727,16.00727,0,0,0,0-27.3125Z"></path></svg>
                     <span class="animate-ping absolute inline-flex h-12 w-12 rounded-full bg-black opacity-50"></span>
                 </div>
 
-                <button class="border-2 border-black focus:outline-none hover:bg-black hover:text-white py-3 rounded-full text-black z-10 text-lg px-10">
+                <button class="border-2 border-black flex-shrink-0 focus:outline-none hover:bg-black hover:text-white  px-6 sm:px-10 py-3 rounded-full text-black text-lg z-10">
                     {{ __('Start') }}
                 </button>
             </div>
