3cc6816a45890dbb35a994753733fe5757ef8693
Milad Mahajna <miladm@toyga.com>
2020-12-14 16:58:58 +0200

    update mail receiver from env file

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -49,8 +49,8 @@ class WebsiteController extends Controller
         Mail::to($client->email)
             ->cc('Academic.golden.test@gmail.com')
             ->send(new QuizResult($quiz));
-
-        Mail::to(['miladmhajna@gmail.com', 'Academic.golden.test@gmail.com'])
+            
+        Mail::to([env('MAIL_OWNER_ADDRESS'), env('MAIL_SERVER_ADDRESS'), env('MAIL_USERNAME')])
             ->cc('Academic.golden.test@gmail.com')
             ->send(new QuizResultAdmin($quiz));
 
